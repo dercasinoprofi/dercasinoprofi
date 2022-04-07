b1ba9b87f5d28d6aea0b0957db08376a2bd460b3
kempo <kemal.aslan@hackuva.de>
2022-04-07 20:35:06 +0200

    content updates

--- .nuxt/components/readme.md
+++ .nuxt/components/readme.md
@@ -6,13 +6,13 @@ You can directly use them in pages and other components without the need to impo
 
 **Tip:** If a component is conditionally rendered with `v-if` and is big, it is better to use `Lazy` or `lazy-` prefix to lazy load.
 
-- `<CardsCard>` | `<cards-card>` (components/cards/Card.vue)
-- `<CardsGroup>` | `<cards-group>` (components/cards/Group.vue)
-- `<CardsHorizontal>` | `<cards-horizontal>` (components/cards/Horizontal.vue)
 - `<CustomFeatureLinks>` | `<custom-feature-links>` (components/custom/FeatureLinks.vue)
 - `<CustomStartpageText>` | `<custom-startpage-text>` (components/custom/StartpageText.vue)
 - `<CustomTopCasinos>` | `<custom-top-casinos>` (components/custom/TopCasinos.vue)
 - `<Footer>` | `<footer>` (components/footer/Footer.vue)
+- `<CardsCard>` | `<cards-card>` (components/cards/Card.vue)
+- `<CardsGroup>` | `<cards-group>` (components/cards/Group.vue)
+- `<CardsHorizontal>` | `<cards-horizontal>` (components/cards/Horizontal.vue)
 - `<NavbarHeaderLogo>` | `<navbar-header-logo>` (components/navbar/HeaderLogo.vue)
 - `<NavbarHeaderStandard>` | `<navbar-header-standard>` (components/navbar/HeaderStandard.vue)
 - `<SliderHeaderSliderControls>` | `<slider-header-slider-controls>` (components/slider/HeaderSliderControls.vue)
--- components/custom/FeatureLinks.vue
+++ components/custom/FeatureLinks.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row pt-5">
       <div class="col-4 pt-5">
-        <div class="card" style="width: 18rem">
+        <div class="card">
           <!-- <img
             src="~/assets/images/logo/logo.png"
             class="card-img-top"
@@ -12,24 +12,22 @@
             Casino Vergleich
           </div>
           <div class="card-body">
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+            <p class="card-text">Die besten Casinos im Test</p>
           </div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">Zum grossen Vergleich</li>
+            <li class="list-group-item">Platz 1. Quasar Gaming</li>
+            <li class="list-group-item">Platz 2: Stargames</li>
+            <li class="list-group-item">Platz 3: Mr. Green</li>
           </ul>
-          <div class="card-body">
+          <!-- <div class="card-body">
             <a href="#" class="card-link">Card link</a>
             <a href="#" class="card-link">Another link</a>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="col-4 pt-5">
-        <div class="card" style="width: 18rem">
+        <div class="card">
           <!-- <img
             src="~/assets/images/logo/logo.png"
             class="card-img-top"
@@ -39,120 +37,119 @@
             Top Boni
           </div>
           <div class="card-body">
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+            <p class="card-text">Sichere dir deinen Bonus</p>
           </div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">Übersicht der Top Boni</li>
+            <li class="list-group-item">Platz 1: 888Casino</li>
+            <li class="list-group-item">Platz 2: Casino Cruise</li>
+            <li class="list-group-item">Platz 3: Platz 3: Casino Club</li>
           </ul>
-          <div class="card-body">
+          <!-- <div class="card-body">
             <a href="#" class="card-link">Card link</a>
             <a href="#" class="card-link">Another link</a>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="col-4 pt-5">
-        <div class="card" style="width: 18rem">
+        <div class="card">
           <!-- <img
             src="~/assets/images/logo/logo.png"
             class="card-img-top"
             alt="..."
           /> -->
+          <div class="card-header cardTitle fw-bold text-light text-center">
+            Spieleangebot
+          </div>
           <div class="card-body">
-            <h5 class="card-title">Spieleangebot</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+            <p class="card-text">Was möchtest du gerne spielen?</p>
           </div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">Top Slot Casinos</li>
+            <li class="list-group-item">Top Poker Casinos</li>
+            <li class="list-group-item">Top Mobile Casinos</li>
+            <li class="list-group-item">Top Live Casinos</li>
           </ul>
-          <div class="card-body">
+          <!-- <div class="card-body">
             <a href="#" class="card-link">Card link</a>
             <a href="#" class="card-link">Another link</a>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="col-4 pt-5">
-        <div class="card" style="width: 18rem">
+        <div class="card">
           <!-- <img
             src="~/assets/images/logo/logo.png"
             class="card-img-top"
             alt="..."
           /> -->
+          <div class="card-header cardTitle fw-bold text-light text-center">
+            Zahlungsmethoden
+          </div>
           <div class="card-body">
-            <h5 class="card-title">Zahlungsmethoden</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+            <!-- <h5 class="card-title"></h5> -->
+            <p class="card-text">Wie kann ich einzahlen/auszahlen?</p>
           </div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">Alle Zahlungsmethoden im Überblick</li>
+            <li class="list-group-item">Casinos mit Kredikarten</li>
+            <li class="list-group-item">Mit Skrill zahlen</li>
           </ul>
-          <div class="card-body">
+          <!-- <div class="card-body">
             <a href="#" class="card-link">Card link</a>
             <a href="#" class="card-link">Another link</a>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="col-4 pt-5">
-        <div class="card" style="width: 18rem">
+        <div class="card">
           <!-- <img
             src="~/assets/images/logo/logo.png"
             class="card-img-top"
             alt="..."
           /> -->
+          <div class="card-header cardTitle fw-bold text-light text-center">
+            Spielregeln
+          </div>
           <div class="card-body">
-            <h5 class="card-title">Spielregeln</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+            <p class="card-text">Alle Casino Spiele mit Anleitung</p>
           </div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">Alles über Poker</li>
+            <li class="list-group-item">BlackJack Regeln erklärt</li>
+            <li class="list-group-item">Wie funktioniert Roulette?</li>
+            <li class="list-group-item">Baccarat Anleitung</li>
           </ul>
-          <div class="card-body">
+          <!-- <div class="card-body">
             <a href="#" class="card-link">Card link</a>
             <a href="#" class="card-link">Another link</a>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="col-4 pt-5">
-        <div class="card" style="width: 18rem">
+        <div class="card">
           <!-- <img
             src="~/assets/images/logo/logo.png"
             class="card-img-top"
             alt="..."
           /> -->
+          <div class="card-header cardTitle fw-bold text-light text-center">
+            Online Slots
+          </div>
           <div class="card-body">
-            <h5 class="card-title">Online Slots</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+            <!-- <h5 class="card-title">Online Slots</h5> -->
+            <p class="card-text">Kostenlos Slots spielen</p>
           </div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">Zur Slot Übersicht</li>
+            <li class="list-group-item">Book of Ra spielen</li>
+            <li class="list-group-item">Jetzt Starburst spielen</li>
+            <li class="list-group-item">Lucky Ladys Charm spielen</li>
           </ul>
-          <div class="card-body">
+          <!-- <div class="card-body">
             <a href="#" class="card-link">Card link</a>
             <a href="#" class="card-link">Another link</a>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
@@ -164,7 +161,7 @@
   width: 100%;
   background-color: green;
   color: #fff;
-  font-size: 14px;
+  font-size: 22px;
   text-transform: capitalize;
 }
 </style>
